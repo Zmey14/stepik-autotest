5c0fc08f22e2e4ebca36eeeb9cbf741ee1ee7345
Zmey14 <mishkaneshishka@gmail.com>
2021-06-20 15:06:55 +0300

    testing send file

---
+++ login.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time
+
+
+class log():
+    login = 'mishkaneshishka@gmail.com'
+    passw = 'Borussia11'
+    
+
+
+link = 'https://lk.sut.ru/cabinet/?login=no'
+browser = webdriver.Chrome()
+browser.get(link)
+
+try:
+    input_log = browser.find_element_by_css_selector('input#users').send_keys(log.login)
+    input_pas = browser.find_element_by_css_selector('input#parole').send_keys(log.passw)
+    button_1 = browser.find_element_by_css_selector('input#logButton').click()
+    time.sleep(2)
+finally:
+    browser.quit()
